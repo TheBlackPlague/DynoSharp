50051178eb11a1f8f7ef0c7d6261448b6f012ac3
Shaheryar Sohail <44123859+TheBlackPlague@users.noreply.github.com>
2021-08-15 01:46:02 +0500

    Parallelize and Optimize OpenCvDetection.

--- DynoSharp/DynoBrain.cs
+++ DynoSharp/DynoBrain.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 using OpenCvSharp;
 using OpenCV = OpenCvSharp.Cv2;
 
@@ -10,54 +11,64 @@ namespace DynoSharp
 
     internal static class DynoBrain
     {
-        
-        /*
-        public const int 
-            CV_8UC1 = 0,
-            CV_8SC1 = 1,
-            CV_16UC1 = 2,
-            CV_16SC1 = 3,
-            CV_32SC1 = 4,
-            CV_32FC1 = 5,
-            CV_64FC1 = 6,
-            CV_8UC2 = 8,
-            CV_8SC2 = 9,
-            CV_16UC2 = 10,
-            CV_16SC2 = 11,
-            CV_32SC2 = 12,
-            CV_32FC2 = 13,
-            CV_64FC2 = 14,
-            CV_8UC3 = 16,
-            CV_8SC3 = 17,
-            CV_16UC3 = 18,
-            CV_16SC3 = 19,
-            CV_32SC3 = 20,
-            CV_32FC3 = 21,
-            CV_64FC3 = 22,
-            CV_8UC4 = 24,
-            CV_8SC4 = 25,
-            CV_16UC4 = 26,
-            CV_16SC4 = 27,
-            CV_32SC4 = 28,
-            CV_32FC4 = 29,
-            CV_64FC4 = 30,
-            CV_8UC5 = 32,
-            CV_8SC5 = 33,
-            CV_16UC5 = 34,
-            CV_16SC5 = 35,
-            CV_32SC5 = 36,
-            CV_32FC5 = 37,
-            CV_64FC5 = 38,
-            CV_8UC6 = 40,
-            CV_8SC6 = 41,
-            CV_16UC6 = 42,
-            CV_16SC6 = 43,
-            CV_32SC6 = 44,
-            CV_32FC6 = 45,
-            CV_64FC6 = 46;
-        */
 
-        private static Mat ReferenceImage = null!;
+        private const double MAX_NORMALIZED_VALUE = 1.0;
+        private const double JUMP_RATIO = 0.25;
+        
+        
+        // public const int 
+        //     CV_8UC1 = 0,
+        //     CV_8SC1 = 1,
+        //     CV_16UC1 = 2,
+        //     CV_16SC1 = 3,
+        //     CV_32SC1 = 4,
+        //     CV_32FC1 = 5,
+        //     CV_64FC1 = 6,
+        //     CV_8UC2 = 8,
+        //     CV_8SC2 = 9,
+        //     CV_16UC2 = 10,
+        //     CV_16SC2 = 11,
+        //     CV_32SC2 = 12,
+        //     CV_32FC2 = 13,
+        //     CV_64FC2 = 14,
+        //     CV_8UC3 = 16,
+        //     CV_8SC3 = 17,
+        //     CV_16UC3 = 18,
+        //     CV_16SC3 = 19,
+        //     CV_32SC3 = 20,
+        //     CV_32FC3 = 21,
+        //     CV_64FC3 = 22,
+        //     CV_8UC4 = 24,
+        //     CV_8SC4 = 25,
+        //     CV_16UC4 = 26,
+        //     CV_16SC4 = 27,
+        //     CV_32SC4 = 28,
+        //     CV_32FC4 = 29,
+        //     CV_64FC4 = 30,
+        //     CV_8UC5 = 32,
+        //     CV_8SC5 = 33,
+        //     CV_16UC5 = 34,
+        //     CV_16SC5 = 35,
+        //     CV_32SC5 = 36,
+        //     CV_32FC5 = 37,
+        //     CV_64FC5 = 38,
+        //     CV_8UC6 = 40,
+        //     CV_8SC6 = 41,
+        //     CV_16UC6 = 42,
+        //     CV_16SC6 = 43,
+        //     CV_32SC6 = 44,
+        //     CV_32FC6 = 45,
+        //     CV_64FC6 = 46;
+
+        private static Mat CactusTemplate = null!;
+        private static Mat OlympicTorchTemplate = null!;
+
+        private static readonly double[] ThresholdValues = 
+        {
+            0.9,
+            0.7
+        };
+        
         private static Mat ScreenFrame = null!;
 
         public static void Loop()
@@ -78,13 +89,11 @@ namespace DynoSharp
 
                 ScreenFrame = screenFrame;
                 // OpenCvDetection();
-
-                /*
-                Window window = new Window("Display", Mat);
-                window.Resize(Mat.Width, Mat.Height);
-                Cv2.WaitKey();
-                window.Dispose();
-                */
+                
+                // Window window = new Window("Display", Mat);
+                // window.Resize(Mat.Width, Mat.Height);
+                // Cv2.WaitKey();
+                // window.Dispose();
 
                 ScreenFrame.Dispose();
                 
@@ -97,67 +106,138 @@ namespace DynoSharp
 
         private static void Startup()
         {
-            ReferenceImage = new Mat(@"///", ImreadModes.Unchanged); // CV_8UC4
-            // Console.WriteLine(ReferenceImage.Type());
+            // Mat Type: CV_8UC4 (4 colors: BGRA, unsigned)
+            Mat tempCactus = new Mat(@"ImageReference/cactus.png", ImreadModes.Unchanged);
+            Mat tempOlympicTorch = new Mat(@"ImageReference/olympic_torch.png", ImreadModes.Unchanged);
+
+            CactusTemplate = tempCactus.CvtColor(ColorConversionCodes.BGRA2GRAY);
+            OlympicTorchTemplate = tempOlympicTorch.CvtColor(ColorConversionCodes.BGRA2GRAY);
+            
+            tempCactus.Dispose();
+            tempOlympicTorch.Dispose();
         }
 
         private static void Shutdown()
         {
-            ReferenceImage.Dispose();
+            CactusTemplate.Dispose();
+            OlympicTorchTemplate.Dispose();
         }
 
         private static void OpenCvDetection()
         {
-            int row = ScreenFrame.Rows - ReferenceImage.Rows + 1;
-            int col = ScreenFrame.Cols - ReferenceImage.Cols + 1;
-            Mat result = new Mat(row, col, MatType.CV_32FC1);
-            
-            Mat grayScreenFrame = ScreenFrame.CvtColor(ColorConversionCodes.BGRA2GRAY);
-            // Can be done before OpenCvDetection() is called.
-            Mat grayReferenceImage = ReferenceImage.CvtColor(ColorConversionCodes.BGRA2GRAY);
-            
-            OpenCV.MatchTemplate(
-                grayScreenFrame, 
-                grayReferenceImage, 
-                result, 
-                TemplateMatchModes.CCoeffNormed
-                );
-            OpenCV.Threshold(result, result, 0.8, 1.0, ThresholdTypes.Tozero);
+            int rowCactus = ScreenFrame.Rows - CactusTemplate.Rows + 1;
+            int colCactus = ScreenFrame.Cols - CactusTemplate.Cols + 1;
+            int rowOlympicTorch = ScreenFrame.Rows - OlympicTorchTemplate.Rows + 1;
+            int colOlympicTorch = ScreenFrame.Cols - OlympicTorchTemplate.Cols + 1;
+            Mat resultCactus = new Mat(rowCactus, colCactus, MatType.CV_32FC1);
+            Mat resultOlympicTorch = new Mat(rowOlympicTorch, colOlympicTorch, MatType.CV_32FC1);
 
-            while (true) {
-                const double threshold = 0.9;
-                
-                OpenCV.MinMaxLoc(
-                    result, 
-                    out _, 
-                    out double maxValue, 
-                    out _, 
-                    out Point maxLocation
+            Mat screenFrameTemplate = ScreenFrame.CvtColor(ColorConversionCodes.BGRA2GRAY);
+
+            (Mat, Mat)[] matchingTemplates = 
+            {
+                (CactusTemplate, resultCactus),
+                (OlympicTorchTemplate, resultOlympicTorch)
+            };
+            
+            Parallel.For(0, matchingTemplates.Length, i =>
+            {
+                OpenCV.MatchTemplate(
+                    // ReSharper disable once AccessToDisposedClosure
+                    screenFrameTemplate,
+                    matchingTemplates[i].Item1,
+                    matchingTemplates[i].Item2,
+                    TemplateMatchModes.CCoeffNormed
+                    );
+
+                OpenCV.Threshold(
+                    matchingTemplates[i].Item2,
+                    matchingTemplates[i].Item2,
+                    ThresholdValues[i],
+                    MAX_NORMALIZED_VALUE,
+                    ThresholdTypes.Tozero
                     );
                 
-                if (maxValue < threshold) break;
+                while (true) {
+                
+                    OpenCV.MinMaxLoc(
+                        matchingTemplates[i].Item2, 
+                        out _, 
+                        out double maxValue, 
+                        out _, 
+                        out Point maxLocation
+                    );
+                
+                    if (maxValue < ThresholdValues[i]) break;
 
-                if (maxLocation.X < ScreenFrame.Width * 0.25) {
-                    KeyboardHandler.PressSpace();
-                    break;
+                    if (maxLocation.X < ScreenFrame.Width * JUMP_RATIO) {
+                        KeyboardHandler.PressSpace();
+                        break;
+                    }
+                
+                    // Fill the result so we don't find the same value again
+                    // when running OpenCV.MinMaxLoc().
+                    OpenCV.FloodFill(
+                        matchingTemplates[i].Item2, 
+                        maxLocation, 
+                        new Scalar(0), 
+                        out _, 
+                        new Scalar(0.1), 
+                        new Scalar(1.0)
+                    );
                 }
                 
-                // Fill the result so we don't find the same value again
-                // when running OpenCV.MinMaxLoc().
-                OpenCV.FloodFill(
-                    result, 
-                    maxLocation, 
-                    new Scalar(0), 
-                    out _, 
-                    new Scalar(0.1), 
-                    new Scalar(1.0)
-                );
-            }
+                matchingTemplates[i].Item2.Dispose();
+            });
+
+            // int row = ScreenFrame.Rows - ReferenceImage.Rows + 1;
+            // int col = ScreenFrame.Cols - ReferenceImage.Cols + 1;
+            // Mat result = new Mat(row, col, MatType.CV_32FC1);
+            
+            // Mat grayScreenFrame = ScreenFrame.CvtColor(ColorConversionCodes.BGRA2GRAY);
+            // Can be done before OpenCvDetection() is called.
+            // Mat grayReferenceImage = ReferenceImage.CvtColor(ColorConversionCodes.BGRA2GRAY);
+            
+            // OpenCV.MatchTemplate(
+            //     grayScreenFrame, 
+            //     grayReferenceImage, 
+            //     result, 
+            //     TemplateMatchModes.CCoeffNormed
+            //     );
+            // OpenCV.Threshold(result, result, 0.8, 1.0, ThresholdTypes.Tozero);
+
+            // while (true) {
+            //     const double threshold = 0.9;
+            //     
+            //     OpenCV.MinMaxLoc(
+            //         result, 
+            //         out _, 
+            //         out double maxValue, 
+            //         out _, 
+            //         out Point maxLocation
+            //         );
+            //     
+            //     if (maxValue < threshold) break;
+            //
+            //     if (maxLocation.X < ScreenFrame.Width * 0.25) {
+            //         KeyboardHandler.PressSpace();
+            //         break;
+            //     }
+            //     
+            //     // Fill the result so we don't find the same value again
+            //     // when running OpenCV.MinMaxLoc().
+            //     OpenCV.FloodFill(
+            //         result, 
+            //         maxLocation, 
+            //         new Scalar(0), 
+            //         out _, 
+            //         new Scalar(0.1), 
+            //         new Scalar(1.0)
+            //     );
+            // }
             
             // Free resources.
-            result.Dispose();
-            grayScreenFrame.Dispose();
-            grayReferenceImage.Dispose();
+            screenFrameTemplate.Dispose();
         }
 
         public static void Benchmark(int iterations)
