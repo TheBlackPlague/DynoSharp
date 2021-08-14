c05cf90c8927e67cd8bbb5d33e015d3a854fb227
Shaheryar Sohail <44123859+TheBlackPlague@users.noreply.github.com>
2021-08-14 21:10:27 +0500

    Implement keyboard emulation.

---
+++ DynoSharp/KeyboardHandler.cs
@@ -0,0 +1,19 @@
+﻿using InputSimulatorStandard;
+using InputSimulatorStandard.Native;
+
+namespace DynoSharp
+{
+
+    internal static class KeyboardHandler
+    {
+
+        private static readonly KeyboardSimulator Simulator = new KeyboardSimulator();
+
+        public static void PressSpace()
+        {
+            Simulator.KeyPress(VirtualKeyCode.SPACE);
+        }
+
+    }
+
+}
